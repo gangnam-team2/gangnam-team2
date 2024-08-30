649eda488d836c1255e18ee52f1ac1e97440c5e6
kkandda <diobobo@naver.com>
2024-08-30 14:46:12 +0900

    manger

--- src/main/java/com/ohgiraffers/book/Application.java
+++ src/main/java/com/ohgiraffers/book/Application.java
@@ -1,5 +1,8 @@
 package com.ohgiraffers.book;
 
+import com.ohgiraffers.book.controller.ManagerController;
+import com.ohgiraffers.book.controller.RequestController;
+
 import java.util.Scanner;
 
 public class Application {
@@ -49,7 +52,7 @@ public class Application {
                 System.out.println("\n== 관리자 메뉴 ==");
                 System.out.println("1. 도서 관리");
                 System.out.println("2. 도서 검색 (연체된 도서 목록 포함)");
-                System.out.println("3. 사용자 관리");
+                System.out.println("3. 사용자 관리"); // 서현준
                 System.out.println("4. 회원탈퇴");
                 System.out.println("0. 로그아웃");
             } else {
@@ -57,7 +60,7 @@ public class Application {
                 System.out.println("1. 도서 검색");
                 System.out.println("2. 대여 및 반납");
                 System.out.println("3. 베스트셀러");
-                System.out.println("4. 도서 요청");
+                System.out.println("4. 도서 요청"); // 서현준
                 System.out.println("5. 마이페이지");
                 System.out.println("6. 회원탈퇴");
                 System.out.println("0. 로그아웃");
@@ -83,17 +86,19 @@ public class Application {
                     }
                     break;
                 case 3:
-                    if (userRole == UserRole.ADMIN) {
-                        UserController.manageUsers(sc);
+                    if (userRole) {
+                        // (관리자 모드) 사용자 관리 / 1. 회원 목록 조회 / 2. 도서를 대여 중인 회원과 그 도서 목록 조회 / 3. 연체 이력이 있는 회원 목록 조회
+                        ManagerController.displayManager();
                     } else {
                         BookController.showBestsellers(sc);
                     }
                     break;
                 case 4:
-                    if (userRole == UserRole.ADMIN) {
+                    if (userRole) {
                         UserController.deleteUser(sc);
                     } else {
-                        BookController.requestBook(sc);
+                        // (사용자 모드) 도서 요청 / 도서관에 없는 책을 요청 도서 목록에 인설트
+                        RequestController.bookRequest();
                     }
                     break;
                 case 5:
