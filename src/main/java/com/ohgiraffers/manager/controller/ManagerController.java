b1048ba8f9af7e7c534c6d9734ea8e633defdb84
diobo <diobobo@naver.com>
2024-09-01 20:26:11 +0900

    사용자 관리

--- src/main/java/com/ohgiraffers/manager/controller/ManagerController.java
+++ src/main/java/com/ohgiraffers/manager/controller/ManagerController.java
@@ -1,17 +1,27 @@
 package com.ohgiraffers.manager.controller;
 
+import com.ohgiraffers.book.dto.BookDTO;
+import com.ohgiraffers.borrowrecord.dto.BorrowRecordDTO;
+import com.ohgiraffers.manager.dao.ManagerDAO;
+import com.ohgiraffers.user.dto.UserDTO;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
+import static com.ohgiraffers.common.JDBCTemplate.getConnection;
+
 public class ManagerController {
-Scanner scr = new Scanner(System.in);
+
+private ManagerDAO managerDAO = new ManagerDAO("src/main/resources/mapper/manRe-query.xml");
 
     public void displayManager(){
-
         System.out.println("사용자 관리 모드 입니다.");
 
         System.out.println("1. 회원들의 정보를 조회합니다.");
-        System.out.println("2. 대여중인 책과 회원들을 조회합니다.");
+        System.out.println("2. 대여 중인 책과 회원들을 조회합니다.");
         System.out.println("3. 현재 연체 중인 회원들을 조회합니다.");
+        Scanner scr = new Scanner(System.in);
         int num = scr.nextInt();
         switch (num){
             case 1: allMembersInfo(); break;
@@ -25,14 +35,32 @@ Scanner scr = new Scanner(System.in);
     }
 
     public void allMembersInfo(){
-
+        List<UserDTO> allMembersInfo;
+        allMembersInfo = managerDAO.selectAllMembersInfo(getConnection());
+        System.out.println("회원 정보 리스트");
+        for (UserDTO member : allMembersInfo){
+            System.out.println(member);
+        }
     }
 
     public void findBookList(){
-
+        List<BookDTO> allBookList;
+        allBookList = managerDAO.selectAllBooksInfo(getConnection());
+        System.out.println("대여 중인 책과 그 회원 목록"); // 현재는 회원은 출력되지 않는다. BookDTO를 사용하였고 JOIN을 써야 할 것 같다.
+        for (BookDTO book : allBookList){
+            System.out.println(book);
+        }
     }
 
     public void memberHistoy(){
+        List<BorrowRecordDTO> lateMember;
+        lateMember = managerDAO.selectMemberHistoy(getConnection());
+        System.out.println("연체 이력이 있는 회원 목록입니다."); // 실제 반납일 > 대여 마감일, 현재를 반영하기 어렵다.
+        for (BorrowRecordDTO member : lateMember){
+            System.out.println(member);
+        }
+
+
 
     }
 
