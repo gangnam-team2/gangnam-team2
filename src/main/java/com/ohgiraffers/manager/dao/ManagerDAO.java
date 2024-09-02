4c6f07239ea9cacc66779bca86e71317a8c2a34e
kkandda <diobobo@naver.com>
2024-09-02 09:39:50 +0900

    사용자 관리 쿼리문 수정

--- src/main/java/com/ohgiraffers/manager/dao/ManagerDAO.java
+++ src/main/java/com/ohgiraffers/manager/dao/ManagerDAO.java
@@ -71,7 +71,9 @@ public class ManagerDAO {
 
             while(rset.next()){
                 BookDTO bookInfo = new BookDTO();
+                UserDTO userInfo = new UserDTO();
                 // 사용자 아이디도 출력해야 하는데, 쿼리문에 조인을 어떻게 써야할 지 모르겠다...
+                userInfo.setUserId(rset.getString("user_id"));
                 bookInfo.setBookCode(rset.getInt("book_code"));
                 bookInfo.setBookTitle(rset.getString("book_title"));
                 bookInfo.setBookAuthor(rset.getString("book_author"));
