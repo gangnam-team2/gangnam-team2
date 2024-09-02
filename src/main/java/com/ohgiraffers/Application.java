462e4120d68b7f1b21b5b66f1dd8e87e88174b85
kkandda <diobobo@naver.com>
2024-09-02 10:58:13 +0900

    사용자 관리 2차 수정

--- src/main/java/com/ohgiraffers/Application.java
+++ src/main/java/com/ohgiraffers/Application.java
@@ -2,6 +2,8 @@ package com.ohgiraffers;
 
 import com.ohgiraffers.book.controller.BestSellersController;
 import com.ohgiraffers.book.controller.BookController;
+import com.ohgiraffers.manager.controller.ManagerController;
+import com.ohgiraffers.request.controller.RequestController;
 import com.ohgiraffers.user.controller.UserController;
 import com.ohgiraffers.user.dao.UserDAO;
 
@@ -54,6 +56,8 @@ public class Application {
 
         BookController bookController = new BookController();
         BestSellersController bestSellersController = new BestSellersController();
+        ManagerController managerController = new ManagerController();
+        RequestController requestController = new RequestController();
         boolean isRunning = true;
 
         while (isRunning) {
@@ -97,7 +101,7 @@ public class Application {
                     break;
                 case 3:
                     if (userRole == true) {
-                        ManagerController.displayManager();
+                        managerController.displayManager();
                     } else {
                         bestSellersController.showBestSellers();
                     }
@@ -107,7 +111,7 @@ public class Application {
                         // 베스트셀러 관리 선택지를 관리자에게 보이지 않도록 제거했습니다.
                         bestSellersController.showBestSellers();
                     } else {
-                        RequestController.bookRequest();
+                        requestController.insertRequestedBook();
                     }
                     break;
                 case 5:
